85d0d95c36af4eb6e67c8b66c9f92cd02dfef47e
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-12 23:34:08 +0800

    Plugin - added in the admin panel list: settings->WP Dupecop

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package Erwin
+ * @version 1.0
+ */
+/*
+Plugin Name: Wordpress Dupe Cop
+Plugin URI: http://fashionsponge.com
+Description: This is the second plugin - I will be developing man!
+Author: Jesus Erwin Suarez
+Version: 1.0
+Author URI: http://erwin.com  
+src:  https://www.youtube.com/watch?v=_nW-h52I7Tg
+*/ 
+
+
+/**
+* Origianl article
+*/
+ 
+/**
+* Spun articles
+*/ 
+ 
+/**
+* Admin page 
+*/  
+function wp_dupe_cop_page() {
+	ob_start();?> 
+ 
+	<!-- Write an html code here.. -->
+
+	<?php  
+}
+ 
+/**
+* Admin tab
+*/    
+function wp_dupe_cop_tab() {
+	add_options_page('wp dupeco', 'WP Dupecop', 'manage_options', 'wpdupecup', 'wp_dupe_cop_page');
+}  
+add_action('admin_menu', 'wp_dupe_cop_tab');
